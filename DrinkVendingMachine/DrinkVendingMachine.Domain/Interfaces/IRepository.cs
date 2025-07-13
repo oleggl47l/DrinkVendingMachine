d92f9d137768f450d6a7f7df3aa33315603f5c6b
olegg <s.gelo2004@gmail.com>
2025-07-13 15:25:39 +0300

    feat: add base Repository

---
+++ DrinkVendingMachine/DrinkVendingMachine.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+
+namespace DrinkVendingMachine.Domain.Interfaces;
+
+public interface IRepository<T> where T : class
+{
+    Task<IEnumerable<T>> GetAllAsync();
+    Task<IEnumerable<T>> GetWhereAsync(Expression<Func<T, bool>> predicate);
+    Task<T?> GetByIdAsync(int id);
+    Task AddAsync(T entity);
+    Task UpdateAsync(T entity);
+    Task DeleteAsync(T entity);
+    Task<bool> ExistsAsync(int id);
+}
---
+++ DrinkVendingMachine/DrinkVendingMachine.Infrastructure/Data/Repositories/BaseRepository.cs
@@ -0,0 +1,50 @@
+﻿using System.Linq.Expressions;
+using DrinkVendingMachine.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace DrinkVendingMachine.Infrastructure.Data.Repositories;
+
+public abstract class BaseRepository<T>(ApplicationDbContext context) : IRepository<T>
+    where T : class
+{
+    private readonly DbSet<T> _dbSet = context.Set<T>();
+
+    public virtual async Task<IEnumerable<T>> GetAllAsync()
+    {
+        return await _dbSet.AsNoTracking().ToListAsync();
+    }
+
+    public virtual async Task<IEnumerable<T>> GetWhereAsync(Expression<Func<T, bool>> predicate)
+    {
+        return await _dbSet.AsNoTracking().Where(predicate).ToListAsync();
+    }
+
+    public virtual async Task<T?> GetByIdAsync(int id)
+    {
+        return await _dbSet.FindAsync(id);
+    }
+
+    public virtual async Task AddAsync(T entity)
+    {
+        await _dbSet.AddAsync(entity);
+        await context.SaveChangesAsync();
+    }
+
+    public virtual async Task UpdateAsync(T entity)
+    {
+        _dbSet.Update(entity);
+        await context.SaveChangesAsync();
+    }
+
+    public virtual async Task DeleteAsync(T entity)
+    {
+        _dbSet.Remove(entity);
+        await context.SaveChangesAsync();
+    }
+
+    public virtual async Task<bool> ExistsAsync(int id)
+    {
+        var entity = await GetByIdAsync(id);
+        return entity != null;
+    }
+}
