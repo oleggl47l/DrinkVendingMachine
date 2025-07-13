4539731fe0b3838fc272000cc1d778fc430d249a
olegg <s.gelo2004@gmail.com>
2025-07-13 16:09:02 +0300

    feat: add drink repository

---
+++ DrinkVendingMachine/DrinkVendingMachine.Domain/Interfaces/IDrinkRepository.cs
@@ -0,0 +1,9 @@
+﻿using DrinkVendingMachine.Domain.Entities;
+
+namespace DrinkVendingMachine.Domain.Interfaces;
+
+public interface IDrinkRepository : IRepository<Drink>
+{
+    Task UpdateQuantityAsync(int id, int quantity, CancellationToken cancellationToken = default);
+    Task<IEnumerable<Drink>> GetByBrandAsync(int brandId, CancellationToken cancellationToken = default);
+}
---
+++ DrinkVendingMachine/DrinkVendingMachine.Infrastructure/Data/Repositories/DrinkRepository.cs
@@ -0,0 +1,35 @@
+﻿using DrinkVendingMachine.Domain.Entities;
+using DrinkVendingMachine.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace DrinkVendingMachine.Infrastructure.Data.Repositories;
+
+public class DrinkRepository(ApplicationDbContext context) : BaseRepository<Drink>(context), IDrinkRepository
+{
+    public override async Task<IEnumerable<Drink>> GetAllAsync(CancellationToken cancellationToken = default) =>
+        await Context.Drinks
+            .AsNoTracking()
+            .Include(d => d.Brand)
+            .ToListAsync(cancellationToken: cancellationToken);
+
+    public override async Task<Drink?> GetByIdAsync(int id, CancellationToken cancellationToken = default) =>
+        await Context.Drinks
+            .AsNoTracking()
+            .Include(d => d.Brand)
+            .FirstOrDefaultAsync(d => d.Id == id, cancellationToken: cancellationToken);
+
+    public async Task UpdateQuantityAsync(int id, int quantity, CancellationToken cancellationToken = default)
+    {
+        var drink = new Drink { Id = id, Quantity = quantity };
+        Context.Drinks.Attach(drink);
+        Context.Entry(drink).Property(d => d.Quantity).IsModified = true;
+        await Context.SaveChangesAsync(cancellationToken);
+    }
+
+    public async Task<IEnumerable<Drink>> GetByBrandAsync(int brandId, CancellationToken cancellationToken = default) =>
+        await Context.Drinks
+            .AsNoTracking()
+            .Include(d => d.Brand)
+            .Where(d => d.BrandId == brandId)
+            .ToListAsync(cancellationToken: cancellationToken);
+}
