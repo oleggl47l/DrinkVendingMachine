3913b76ec4e68e3d2df0119451eea17f4ae48744
olegg <s.gelo2004@gmail.com>
2025-07-14 02:16:06 +0300

    feat: add coin repo

---
+++ DrinkVendingMachine/src/DrinkVendingMachine.Domain/Interfaces/ICoinRepository.cs
@@ -0,0 +1,5 @@
+﻿using DrinkVendingMachine.Domain.Entities;
+
+namespace DrinkVendingMachine.Domain.Interfaces;
+
+public interface ICoinRepository : IRepository<Coin>;
---
+++ DrinkVendingMachine/src/DrinkVendingMachine.Infrastructure/Data/Repositories/CoinRepository.cs
@@ -0,0 +1,6 @@
+﻿using DrinkVendingMachine.Domain.Entities;
+using DrinkVendingMachine.Domain.Interfaces;
+
+namespace DrinkVendingMachine.Infrastructure.Data.Repositories;
+
+public class CoinRepository(ApplicationDbContext context) : BaseRepository<Coin>(context), ICoinRepository;
